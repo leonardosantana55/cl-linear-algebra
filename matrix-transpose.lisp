316e16a8f77bf37c4ff3fcd2ed2349fcdb87ef9e
leonardo-trabalho <analistaleonardosantana55@gmail.com>
2025-09-23 14:03:11 -0300

    'lol'

--- matrix-transpose.lisp
+++ matrix-transpose.lisp
@@ -5,20 +5,13 @@
   (let* ((m (array-dimension input 0))
          (n (array-dimension input 1))
          (output (make-array (list n m) :initial-element nil)))
-    (dotimes (i m)
-      (dotimes (j n)
+    (dotimes (i n)
+      (dotimes (j m)
         (setf (aref output i j)
               (aref input j i))))
     output))
 
 (defparameter *m-t* (matrix-transpose *matrix*))
-
-
-(+ (* (aref *vector* 0) (aref *m-t* 0 0))
-   (* (aref *vector* 1) (aref *m-t* 1 0)))
-
-(+ (* (aref *vector* 0) (aref *m-t* 0 1))
-   (* (aref *vector* 1) (aref *m-t* 1 1)))
 
 (defun matrix-multiplication (matrix vector)
   (let ((m-t (matrix-transpose matrix))
@@ -31,31 +24,13 @@
               (+ (* (aref vector j) (aref m-t j i))
                  (aref output i)))))
     output))
-
 (matrix-multiplication *matrix* *vector*)
 
+(defun vector-multiplication (vector scalar)
+  (let ((output (make-array (array-total-size vector) :initial-element 0)))
+    (dotimes (i (array-total-size vector))
+      (setf (aref output i)
+            (* scalar (aref vector i))))
+    output))
+(vector-multiplication *vector* 2)
 
-(defun matrix-multiplication (matrix vector)
-  (let* ((m (array-dimension matrix 0))
-        (n (array-dimension matrix 1))
-        (v (array-total-size vector))
-        (output (make-array v)))
-    (if (= m v)
-        (dotimes (i v)
-          (dotimes (j n)
-            (setf (aref output n)
-                  (+ (aref output n)
-                     (* (aref matrix i j)
-                        (aref vector i)))))))))
-
-
-(matrix-multiplication *matrix* *vector*)
-
- 
-;;eu vou multiplcar vector vezes 
-
-
-(aref *vector* 0)
-
-
-(make-array (list 2 3))
