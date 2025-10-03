7bce7e3b4bd0da49f914cf5738a0b3adfb6fecdc
leonardosantana55 <analistaleonardosantana55@gmail.com>
2025-10-03 18:04:37 -0300

    emacs

--- matrix-transpose.lisp
+++ matrix-transpose.lisp
@@ -34,6 +34,13 @@
     output))
 (vector-scalar-multiplication *vector* 2)
 
+(defun vector-unit (vector)
+  (vector-scalar-multiplication
+   vector
+   (/ 1 (vector-magnitude vector))))
+(vector-magnitude (vector-unit (vector 2 3)))
+
+
 (defun dot-product (v1 v2)
   (let ((lst nil))
     (dotimes (i (array-total-size v1) (apply #'+ lst))
@@ -53,7 +60,7 @@
     output))
 (defparameter *m-t* (matrix-transpose *matrix*))
 
-(defun matrix-multiplication (matrix vector)
+(defun matrix-vector-multiplication (matrix vector &key (round nil))
   (let ((m-t (matrix-transpose matrix))
         (m (array-dimension matrix 0))
         (n (array-dimension matrix 1))
@@ -63,33 +70,52 @@
         (setf (aref output i)
               (+ (* (aref vector j) (aref m-t j i))
                  (aref output i)))))
-    output))
-(matrix-multiplication *matrix* *vector*)
+    (if (eql round nil)
+        output
+        (dotimes (i (array-total-size output) output)
+          (setf (aref output i)
+                (round (aref output i)))))))
+(matrix-vector-multiplication *matrix* *vector*)
 
+
+(defun (vector)
+  (dotimes (i (array-total-size v) v)
+    (setf (aref v i) (round (aref v i)))))
 
 (dot-product *vector* *vector*)
 (* (vector-magnitude *vector*)
    (vector-magnitude *vector*)
    (cos (vector-angle *vector* *vector*)))
 
+(defun matrix-rotation (angle)
+  (make-array '(2 2) :initial-contents `((,(cos angle) ,(asin angle))
+                                         (,(sin angle) ,(cos angle)))))
+(matrix-rotation 1)
 
 
-(defparameter *p-matrix* (make-array '(16 16) :initial-element "  ."))
-(defparameter *p-vector* (vector 8 8))
-(defparameter *r-vector* (vector 2 2))
-(defparameter *r-matrix* (make-array '(2 2) :initial-contents '((-1 0) (0 -1))))
+;; TESTs
 
-(defun print-matrix (matrix)
-  (dotimes (i (array-dimension matrix 0))
-    (dotimes (j (array-dimension matrix 1))
-      (format t "~a" (aref matrix i j)))
-    (format t "~%"))
-  (format t "~%~a~%" *p-vector*))
+(dotimes (i 4 nil)
+  (print (matrix-)))
+
+(setf *vector* (matrix-vector-multiplication (matrix-rotation 1) *vector* :round t))
+
+;; (defparameter *p-matrix* (make-array '(16 16) :initial-element "  ."))
+;; (defparameter *p-vector* (vector 8 8))
+;; (defparameter *r-vector* (vector 2 2))
+;; (defparameter *r-matrix* (make-array '(2 2) :initial-contents '((-1 0) (0 -1))))
+
+;; (defun print-matrix (matrix)
+;;   (dotimes (i (array-dimension matrix 0))
+;;     (dotimes (j (array-dimension matrix 1))
+;;       (format t "~a" (aref matrix i j)))
+;;     (format t "~%"))
+;;   (format t "~%~a~%" *p-vector*))
 
 
-(dotimes (i 5 nil)
-  (print *r-vector*)
-  (setf *r-vector* (matrix-multiplication *r-matrix* *r-vector*)))
+;; (dotimes (i 5 nil)
+;;   (print *r-vector*)
+;;   (setf *r-vector* (matrix-vector-multiplication *r-matrix* *r-vector*)))
 
 ;; (dotimes (i 10 nil)
 ;;   (print-matrix *p-matrix*)
@@ -98,7 +124,7 @@
 ;;   (setf (aref *p-matrix* (aref *p-vector* 0) (aref *p-vector* 1)) "  .")
 ;;   ;; soma o vector raio ao vector p
 ;;   (setf *p-vector* (vector-sum *p-vector* *r-vector*))
-;;   (setf *r-vector* (matrix-multiplication *r-matrix* *r-vector*))
+;;   (setf *r-vector* (matrix-vector-multiplication *r-matrix* *r-vector*))
 ;;   ;; transforma o vetor raio
 ;;   (sleep 1))
 
